9feb87f19857a985cc52917c899382c0c3c3af1d
ibrahimEthemxd <ibrhm8581@gmail.com>
2024-11-19 17:48:48 +0300

    Context API

--- E-ticaret/frontend/src/components/Products/Products.jsx
+++ E-ticaret/frontend/src/components/Products/Products.jsx
@@ -1,9 +1,9 @@
+import { useEffect, useState } from "react";
 import ProductItem from "./ProductItem";
-import { useState } from "react";
 import Slider from "react-slick";
 import PropTypes from "prop-types";
-import ProductsData from "../../data.json";
 import "./Products.css";
+import { message } from "antd";
 
 function NextBtn({ onClick }) {
   return (
@@ -12,9 +12,11 @@ function NextBtn({ onClick }) {
     </button>
   );
 }
+
 NextBtn.propTypes = {
   onClick: PropTypes.func,
 };
+
 function PrevBtn({ onClick }) {
   return (
     <button className="glide__arrow glide__arrow--left" onClick={onClick}>
@@ -22,13 +24,33 @@ function PrevBtn({ onClick }) {
     </button>
   );
 }
+
 PrevBtn.propTypes = {
   onClick: PropTypes.func,
 };
 
 const Products = () => {
-  const [products] = useState(ProductsData);
-  const [cartItems, setCartItems] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/api/products`);
+
+        if (response.ok) {
+          const data = await response.json();
+          setProducts(data);
+        } else {
+          message.error("Veri getirme başarısız.");
+        }
+      } catch (error) {
+        console.log("Veri hatası:", error);
+      }
+    };
+    fetchProducts();
+  }, [apiUrl]);
 
   const sliderSettings = {
     dots: false,
@@ -54,9 +76,9 @@ const Products = () => {
       },
     ],
   };
+
   return (
     <section className="products">
-      Cart Sayısı: {cartItems.length}
       <div className="container">
         <div className="section-title">
           <h2>Featured Products</h2>
@@ -65,11 +87,7 @@ const Products = () => {
         <div className="product-wrapper product-carousel">
           <Slider {...sliderSettings}>
             {products.map((product) => (
-              <ProductItem
-                productItem={product}
-                setCartItems={setCartItems}
-                key={product.id}
-              />
+              <ProductItem productItem={product} key={product._id} />
             ))}
           </Slider>
         </div>
@@ -77,4 +95,5 @@ const Products = () => {
     </section>
   );
 };
+
 export default Products;
--- E-ticaret/frontend/src/main.jsx
+++ E-ticaret/frontend/src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import MainLayout from "./layouts/MainLayout";
 import App from "./App.jsx";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./index.css";
+import CartProvider from "./context/CardProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <CartProvider>
     <MainLayout>
       <App />
     </MainLayout>
-  </StrictMode>
+  </CartProvider>
 );
