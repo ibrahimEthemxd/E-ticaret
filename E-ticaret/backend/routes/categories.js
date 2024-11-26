211c0de81f002fe6cecc7958289caeeeef77d726
ibrahimEthemxd <ibrhm8581@gmail.com>
2024-11-26 13:21:53 +0300

    Kategori güncelleme

--- E-ticaret/backend/routes/categories.js
+++ E-ticaret/backend/routes/categories.js
@@ -45,6 +45,31 @@ router.get("/:categoryId", async (req, res) => {
     }
 });
 
+//? Kategori güncelleme (Update)
+router.put("/:categoryId", async (req, res) => {
+    try {
+      const categoryId = req.params.categoryId;
+      const updates = req.body;
+  
+      const existingCategory = await Category.findById(categoryId);
+  
+      if (!existingCategory) {
+        return res.status(404).json({ error: "Category not found." });
+      }
+  
+      const updatedCategory = await Category.findByIdAndUpdate(
+        categoryId,
+        updates,
+        { new: true }
+      );
+  
+      res.status(200).json(updatedCategory);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Server error." });
+    }
+  });
+
 
 //!Tüm kategorileri getirme
 router.get("/", async (req, res) => {
