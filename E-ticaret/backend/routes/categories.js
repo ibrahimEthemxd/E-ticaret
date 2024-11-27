e50b13b891a2c907edf7e7830c1839345796876a
ibrahimEthemxd <ibrhm8581@gmail.com>
2024-11-27 11:32:31 +0300

    User Model Oluşturma

--- E-ticaret/backend/routes/categories.js
+++ E-ticaret/backend/routes/categories.js
@@ -48,44 +48,44 @@ router.get("/:categoryId", async (req, res) => {
 //? Kategori güncelleme (Update)
 router.put("/:categoryId", async (req, res) => {
     try {
-      const categoryId = req.params.categoryId;
-      const updates = req.body;
-  
-      const existingCategory = await Category.findById(categoryId);
-  
-      if (!existingCategory) {
-        return res.status(404).json({ error: "Category not found." });
-      }
-  
-      const updatedCategory = await Category.findByIdAndUpdate(
-        categoryId,
-        updates,
-        { new: true }
-      );
-  
-      res.status(200).json(updatedCategory);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: "Server error." });
-    }
-  });
+        const categoryId = req.params.categoryId;
+        const updates = req.body;
 
-  //? Kategori silme (Delete)
+        const existingCategory = await Category.findById(categoryId);
+
+        if (!existingCategory) {
+            return res.status(404).json({ error: "Category not found." });
+        }
+
+        const updatedCategory = await Category.findByIdAndUpdate(
+            categoryId,
+            updates,
+            { new: true }
+        );
+
+        res.status(200).json(updatedCategory);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Server error." });
+    }
+});
+
+//?    Kategori silme (Delete)
 router.delete("/:categoryId", async (req, res) => {
     try {
-      const categoryId = req.params.categoryId;
-  
-      const deletedCategory = await Category.findByIdAndRemove(categoryId);
-  
-      if (!deletedCategory) {
-        return res.status(404).json({ error: "Category not found." });
-      }
-  
-      res.status(200).json(deletedCategory);
+        const categoryId = req.params.categoryId;
+
+        const deletedCategory = await Category.findByIdAndRemove(categoryId);
+
+        if (!deletedCategory) {
+            return res.status(404).json({ error: "Category not found." });
+        }
+
+        res.status(200).json(deletedCategory);
     } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: "Server error." });
+        console.log(error);
+        res.status(500).json({ error: "Server error." });
     }
-  });
+});
 
 module.exports = router;  
