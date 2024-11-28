7a8b6ca28279d555e0cb8efc0541b1ee5f6a49d6
ibrahimEthemxd <ibrhm8581@gmail.com>
2024-11-28 21:44:05 +0300

    Ürünleri Getirme

--- E-ticaret/backend/routes/products.js
+++ E-ticaret/backend/routes/products.js
@@ -14,4 +14,33 @@ router.post("/", async (req, res) => {
     }
   });
 
+  //? Tüm ürünleri getirme (Read - All)
+router.get("/", async (req, res) => {
+    try {
+      const products = await Product.find();
+  
+      res.status(200).json(products);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Server error." });
+    }
+  });
+  
+  //? Belirli bir ürünü getirme (Read - Single)
+  router.get("/:productId", async (req, res) => {
+    try {
+      const productId = req.params.productId;
+      const product = await Product.findById(productId);
+  
+      if (!product) {
+        return res.status(404).json({ error: "Product not found." });
+      }
+  
+      res.status(200).json(product);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Server error." });
+    }
+  });
+
 module.exports = router; 
