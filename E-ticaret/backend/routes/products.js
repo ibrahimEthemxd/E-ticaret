553073deb9773616f04f1a29bb10353b32b43132
ibrahimEthemxd <ibrhm8581@gmail.com>
2024-11-28 21:40:34 +0300

    Yeni Ürün oluşturma

--- E-ticaret/backend/routes/products.js
+++ E-ticaret/backend/routes/products.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const Product = require("../models/Product.js");
 
-//!Tüm ürünleri getirme
-router.get("/", async (req, res) => {
-    res.send("Ürünler getirildi");
-})
+//? Yeni bir ürün oluşturma (Create)
+router.post("/", async (req, res) => {
+    try {
+      const newProduct = new Product(req.body);
+      await newProduct.save();
+      res.status(201).json(newProduct);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Server error." });
+    }
+  });
 
 module.exports = router; 
