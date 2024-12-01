246eb67e4d4cbfbc2d47b7decb906a59ad2fbbb9
ibrahimEthemxd <ibrhm8581@gmail.com>
2024-12-01 13:40:31 +0300

    register input alma

--- E-ticaret/frontend/src/components/Auth/Register.jsx
+++ E-ticaret/frontend/src/components/Auth/Register.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+
 const Register = () => {
+  const [formData, setFromData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInputChance = (e) => {
+    const { name, value } = e.target;
+    setFromData({ ...formData, [name]: value });
+  };
+
   return (
     <div className="account-column">
       <h2>Kayıt Ol</h2>
@@ -8,7 +21,7 @@ const Register = () => {
             <span>
               Kullanıcı Adı <span className="required">*</span>
             </span>
-            <input type="text" />
+            <input type="text" onChange={handleInputChance} name="username" />
           </label>
         </div>
         <div>
@@ -16,7 +29,7 @@ const Register = () => {
             <span>
               E-posta Adresi <span className="required">*</span>
             </span>
-            <input type="email" />
+            <input type="email" onChange={handleInputChance} name="email" />
           </label>
         </div>
         <div>
@@ -24,7 +37,11 @@ const Register = () => {
             <span>
               Şifre <span className="required">*</span>
             </span>
-            <input type="password" />
+            <input
+              type="password"
+              onChange={handleInputChance}
+              name="password"
+            />
           </label>
         </div>
         <div className="privacy-policy-text remember">
