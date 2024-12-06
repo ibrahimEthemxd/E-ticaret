974233d0e400b266da9b341efa57760b18aa3919
ibrahimEthemxd <ibrhm8581@gmail.com>
2024-12-06 17:54:01 +0300

    Yorum müşteri bilgileri corrected

--- E-ticaret/frontend/src/components/Reviews/Reviews.jsx
+++ E-ticaret/frontend/src/components/Reviews/Reviews.jsx
@@ -2,8 +2,38 @@ import ReviewForm from "./ReviewForm";
 import ReviewItem from "./ReviewItem";
 import PropTypes from "prop-types";
 import "./Reviews.css";
+import { useEffect, useState } from "react";
+import { message } from "antd";
 
 const Reviews = ({ active, singleProduct, setSingleProduct }) => {
+  const [users, setUsers] = useState([]);
+  const apiUrl = import.meta.env.VITE_API_BASE_URL;
+  const thisReview = [];
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/api/users`);
+        if (response.ok) {
+          const data = await response.json();
+          setUsers(data);
+        } else {
+          message.error("Veri getirme başarısız.");
+        }
+      } catch (error) {
+        console.log("Veri hatası:", error);
+      }
+    };
+    fetchUsers();
+  }, [apiUrl]);
+
+  singleProduct.reviews.forEach((review) => {
+    const matchingUsers = users?.filter((user) => user._id === review.user);
+    matchingUsers.forEach((matchingUser) => {
+      thisReview.push
+    })
+  });
+
   return (
     <div className={`tab-panel-reviews ${active}`}>
       {singleProduct.reviews.length > 0 ? (
