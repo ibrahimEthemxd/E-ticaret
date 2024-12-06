1e9ad1c115e600236feeafabfa440de121fdc934
ibrahimEthemxd <ibrhm8581@gmail.com>
2024-12-06 18:32:50 +0300

    alright

--- E-ticaret/frontend/src/components/Cart/Cart.jsx
+++ E-ticaret/frontend/src/components/Cart/Cart.jsx
@@ -1,24 +1,44 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/CartProvider";
 import "./Cart.css";
 import CartCoupon from "./CartCoupon";
 import CartProgress from "./CartProgress";
 import CartTable from "./CartTable";
 import CartTotals from "./CartTotals";
+import { useNavigate } from "react-router-dom";
+
 const Cart = () => {
+  const { cartItems } = useContext(CartContext);
+
+  const navigate = useNavigate();
+
   return (
     <section className="cart-page">
       <div className="container">
-        <div className="cart-page-wrapper">
-          <form className="cart-form">
-            <CartProgress />
-            <div className="shop-table-wrapper">
-              <CartTable />
-              <CartCoupon />
+        {cartItems.length > 0 ? (
+          <div className="cart-page-wrapper">
+            <form className="cart-form">
+              <CartProgress />
+              <div className="shop-table-wrapper">
+                <CartTable />
+                <CartCoupon />
+              </div>
+            </form>
+            <div className="cart-collaterals">
+              <CartTotals />
             </div>
-          </form>
-          <div className="cart-collaterals">
-            <CartTotals />
           </div>
-        </div>
+        ) : (
+          <div className="cart-no-product">
+            <h2>Sepetinde ürün bulunmamaktadır.</h2>
+            <button
+              className="btn btn-primary btn-lg"
+              onClick={() => navigate(`/shop`)}
+            >
+              Alışverişe Başla
+            </button>
+          </div>
+        )}
       </div>
     </section>
   );
